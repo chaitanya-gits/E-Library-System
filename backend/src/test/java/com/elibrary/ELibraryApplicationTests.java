68db5c3bfb7b5c0c98f28e5315b2745eddcf3d0d
chaitaanya10 <chaitanyavalluru999@gmail.com>
2026-01-04 16:54:12 +0530

    fix: explicitly exclude JPA/DataSource in ELibraryApplicationTests annotation

--- backend/src/test/java/com/elibrary/ELibraryApplicationTests.java
+++ backend/src/test/java/com/elibrary/ELibraryApplicationTests.java
@@ -1,9 +1,20 @@
 package com.elibrary;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 
 @SpringBootTest
+@EnableAutoConfiguration(exclude = {
+        DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class,
+        JpaRepositoriesAutoConfiguration.class
+})
 class ELibraryApplicationTests {
 
     @Test
