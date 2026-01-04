b5fed06d549ffff643550ba8c791fe8c281b4fc5
chaitaanya10 <chaitanyavalluru999@gmail.com>
2026-01-04 19:39:14 +0530

    Fix: Add missing SpringBootTest import in ELibraryApplicationTests

--- backend/src/test/java/com/elibrary/ELibraryApplicationTests.java
+++ backend/src/test/java/com/elibrary/ELibraryApplicationTests.java
@@ -2,8 +2,10 @@ package com.elibrary;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@ActiveProfiles("test")
 class ELibraryApplicationTests {
 
     @Test
