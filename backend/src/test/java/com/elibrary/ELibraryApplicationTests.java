09478ae7662e845171bb5baac2a9ff0698c3b59f
chaitaanya10 <chaitanyavalluru999@gmail.com>
2026-01-07 19:49:21 +0530

    Update files

--- backend/src/test/java/com/elibrary/ELibraryApplicationTests.java
+++ backend/src/test/java/com/elibrary/ELibraryApplicationTests.java
@@ -4,8 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
-@ActiveProfiles("test")
+@SpringBootTest
+@ActiveProfiles("dev")
 class ELibraryApplicationTests {
 
     @Test
